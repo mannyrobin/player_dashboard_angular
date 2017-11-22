dc22bc57f7be8b1438a7e83db03644f3464bee41
mannyrobin <69haiqian@gmail.com>
2017-11-22 17:16:30 +0300

    SM-19 + initialize layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {DevExpressModule} from './devexpress';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LayoutComponent } from './layout/layout.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SideBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
