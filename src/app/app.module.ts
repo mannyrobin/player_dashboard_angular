033ba85c3ec8e572e59f13e78a05b1933324a3af
mannyrobin <69haiqian@gmail.com>
2017-11-23 14:42:19 +0300

    SM-20 + ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {DevExpressModule} from './devexpress';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -11,10 +18,15 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    DevExpressModule.imports
-  ],
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
+],
   providers: [],
   bootstrap: [AppComponent]
 })
