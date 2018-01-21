507165bbd51f6f77f58cf4e3ec1e952d18302152
mannyrobin <69haiqian@gmail.com>
2018-01-21 18:53:09 +0300

    SM-46 ! Build prod

--- src/app/pages/groups/group-person/group-person.component.ts
+++ src/app/pages/groups/group-person/group-person.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GroupPerson } from '../../../data/remote/model/group/group-person';
 
 @Component({
   selector: 'app-group-person',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupPersonComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public groupPerson: GroupPerson;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
