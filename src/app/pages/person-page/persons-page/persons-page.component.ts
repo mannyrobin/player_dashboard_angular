3f60d1ae94cf28c594cb0d226a52b3841aea73a1
mannyrobin <69haiqian@gmail.com>
2018-01-31 16:35:37 +0300

    SM-48 ~ Persons page

--- src/app/pages/person-page/persons-page/persons-page.component.ts
+++ src/app/pages/person-page/persons-page/persons-page.component.ts
@@ -1,12 +1,11 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ParticipantRestApiService} from '../../../data/remote/rest-api/participant-rest-api.service';
-import {Person} from '../../../data/remote/model/person';
 import {DxTextBoxComponent} from 'devextreme-angular';
 import {GroupQuery} from '../../../data/remote/rest-api/query/group-query';
 import {PropertyConstant} from '../../../data/local/property-constant';
-import {AppHelper} from '../../../utils/app-helper';
 import {PageQuery} from '../../../data/remote/rest-api/page-query';
 import {PersonQuery} from '../../../data/remote/rest-api/query/person-query';
+import {PersonViewModel} from '../../../data/local/view-model/person-view-model';
 
 @Component({
   selector: 'app-persons-page',
@@ -18,13 +17,13 @@ export class PersonsPageComponent implements OnInit, AfterViewInit {
   @ViewChild('searchDxTextBoxComponent')
   public searchDxTextBoxComponent: DxTextBoxComponent;
 
-  public persons: Person[];
+  public personViewModels: PersonViewModel[];
 
   private _searchText: string;
   private readonly _personQuery: PersonQuery;
 
   constructor(private _participantRestApiService: ParticipantRestApiService) {
-    this.persons = [];
+    this.personViewModels = [];
     this._searchText = '';
 
     this._personQuery = new GroupQuery();
@@ -52,7 +51,14 @@ export class PersonsPageComponent implements OnInit, AfterViewInit {
     this._personQuery.fullName = this._searchText;
 
     const pageContainer = await this._participantRestApiService.getPersons(this._personQuery);
-    this.persons = AppHelper.pushItemsInList(from, this.persons, pageContainer);
-  }
+    for (const person of pageContainer.list) {
+      // TODO: Get base user role
+      const baseGroupPerson = await this._participantRestApiService.getBaseGroup({
+        id: person.id,
+        userRoleId: 10
+      });
 
+      this.personViewModels.push(new PersonViewModel(person, baseGroupPerson.group, this._participantRestApiService));
+    }
+  }
 }
