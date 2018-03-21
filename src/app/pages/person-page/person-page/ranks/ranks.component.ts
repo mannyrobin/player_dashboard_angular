88ea90c2957890cc2e67a089dc86e8664116be9a
mannyrobin <69haiqian@gmail.com>
2018-03-21 19:06:14 +0300

    SM-92 + ranks list, + remove rank

--- src/app/pages/person-page/person-page/ranks/ranks.component.ts
+++ src/app/pages/person-page/person-page/ranks/ranks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ParticipantRestApiService } from '../../../../data/remote/rest-api/participant-rest-api.service';
+import { PersonService } from '../person.service';
+import { PersonRank } from '../../../../data/remote/model/person-rank';
 
 @Component({
   selector: 'app-ranks',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RanksComponent implements OnInit {
 
-  constructor() { }
+  public readonly isEditAllow: boolean;
+  public personRanks: PersonRank[];
 
-  ngOnInit() {
+  constructor(private _personService: PersonService,
+              private _participantRestApiService: ParticipantRestApiService) {
+    this.isEditAllow = _personService.shared.isEditAllow;
+  }
+
+  async ngOnInit() {
+    this.personRanks = await this._participantRestApiService.getRanks({id: this._personService.shared.person.id});
+  }
+
+  async editRank(personRank: PersonRank) {
+    //todo
+  }
+
+  async removeRank(personRank: PersonRank) {
+    await this._participantRestApiService.removeRank({rankId: personRank.rank.id});
+    this.clearPersonRank(personRank);
+  }
+
+  private clearPersonRank(personRank: PersonRank) {
+    personRank.assignee = null;
+    personRank.date = null;
+    personRank.orderNumber = null;
+    personRank.certificateNumber = null;
   }
 
 }
