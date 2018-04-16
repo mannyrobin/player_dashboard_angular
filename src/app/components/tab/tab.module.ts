705681563af06eee662fc05ed30751fe1fc03ac7
mannyrobin <69haiqian@gmail.com>
2018-04-16 15:57:19 +0300

    SM-112 ! import

--- src/app/components/tab/tab.module.ts
+++ src/app/components/tab/tab.module.ts
@@ -1,17 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TabComponent} from './tab.component';
-import {RouterModule} from '@angular/router';
-import {TranslateModule} from '@ngx-translate/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabComponent } from './tab.component';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { ButtonsModule } from "../buttons/buttons.module";
-import { FormsModule } from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([]),
     TranslateModule.forChild(),
-    FormsModule,
     ButtonsModule
   ],
   declarations: [TabComponent],
