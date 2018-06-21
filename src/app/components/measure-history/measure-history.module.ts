fde25eb81973d96e94c6d6c65f0da144700ccc6c
mannyrobin <69haiqian@gmail.com>
2018-06-21 13:21:38 +0300

    SM-156 ~ MeasureHistory and Connections

--- src/app/components/measure-history/measure-history.module.ts
+++ src/app/components/measure-history/measure-history.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ChartHistoryComponent } from './chart-history/chart-history.component';
-import { TableHistoryComponent } from './table-history/table-history.component';
-import { MeasureHistoryComponent } from './measure-history.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { DxDateBoxModule } from 'devextreme-angular';
-import { FormsModule } from '@angular/forms';
-import { TabModule } from '../tab/tab.module';
-import { InfiniteListModule } from '../infinite-list/infinite-list.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MeasureHistoryService } from './measure-history.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChartHistoryComponent} from './chart-history/chart-history.component';
+import {TableHistoryComponent} from './table-history/table-history.component';
+import {MeasureHistoryComponent} from './measure-history.component';
+import {TranslateModule} from '@ngx-translate/core';
+import {DxDateBoxModule} from 'devextreme-angular';
+import {FormsModule} from '@angular/forms';
+import {TabModule} from '../tab/tab.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MeasureHistoryService} from './measure-history.service';
+import {NgxVirtualScrollModule} from '../ngx-virtual-scroll/ngx-virtual-scroll.module';
 
 @NgModule({
   imports: [
@@ -18,7 +18,7 @@ import { MeasureHistoryService } from './measure-history.service';
     TranslateModule.forChild(),
     DxDateBoxModule,
     TabModule,
-    InfiniteListModule,
+    NgxVirtualScrollModule,
     NgbModule
   ],
   declarations: [
--- src/app/data/remote/bean/page-container.ts
+++ src/app/data/remote/bean/page-container.ts
@@ -3,10 +3,4 @@ export class PageContainer<T> {
   size: number;
   total: number;
   list: T[];
-
-  constructor(list: T[]) {
-    this.list = list;
-    this.size = this.total = list.length;
-  }
-
 }
