684470532014adf9aff93837fb3981d16fbc8297
mannyrobin <69haiqian@gmail.com>
2018-07-12 18:48:47 +0300

    SM-165 ~ Edit person groups

--- src/app/pages/person-page/person-page/my-groups/my-groups.component.ts
+++ src/app/pages/person-page/person-page/my-groups/my-groups.component.ts
@@ -3,6 +3,12 @@ import {AppHelper} from '../../../../utils/app-helper';
 import {ParticipantRestApiService} from '../../../../data/remote/rest-api/participant-rest-api.service';
 import {GroupPerson} from '../../../../data/remote/model/group/group-person';
 import {PersonService} from '../person.service';
+import {ModalSelectPageComponent} from '../../../../components/modal-select-page/modal-select-page.component';
+import {PageContainer} from '../../../../data/remote/bean/page-container';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {AuthorizationService} from '../../../../shared/authorization.service';
+import {NamedObjectItemComponent} from '../../../../components/named-object-item/named-object-item.component';
+import {ListRequest} from '../../../../data/remote/request/list-request';
 
 @Component({
   selector: 'app-my-groups',
@@ -15,7 +21,9 @@ export class MyGroupsComponent implements OnInit {
 
   constructor(private _participantRestApiService: ParticipantRestApiService,
               private _appHelper: AppHelper,
-              private _personService: PersonService) {
+              private _personService: PersonService,
+              private _authorizationService: AuthorizationService,
+              private _modalService: NgbModal) {
   }
 
   async ngOnInit() {
@@ -25,9 +33,30 @@ export class MyGroupsComponent implements OnInit {
     }
   }
 
-  public onEdit = () => {
-    // TODO: Add modal for edit my groups
-    console.log('onEdit');
+  public onEdit = async () => {
+    const person = await this._authorizationService.getPerson();
+    const groups = (await this._participantRestApiService.getGroupPersons({personId: person.id})).map(x => x.group);
+
+    const ref = this._modalService.open(ModalSelectPageComponent, {size: 'lg'});
+    const componentInstance = ref.componentInstance as ModalSelectPageComponent<any>;
+    componentInstance.headerNameKey = 'edit';
+    componentInstance.component = NamedObjectItemComponent;
+    componentInstance.getItems = async pageQuery => {
+      const items = groups.filter(x => x.name.toString().toLowerCase().indexOf(pageQuery.name) > -1);
+      const pageContainer = new PageContainer();
+      pageContainer.from = 0;
+      pageContainer.size = items.length;
+      pageContainer.total = items.length;
+      pageContainer.list = items;
+      return pageContainer;
+    };
+    componentInstance.onSave = async selectedItems => {
+      this.groupPersons = await this._participantRestApiService.updateGroupPersons(new ListRequest(selectedItems), {}, {personId: this._personService.personViewModel.data.id});
+      ref.dismiss();
+    };
+
+    const selectedGroups = (await this._participantRestApiService.getGroupPersons({personId: this._personService.personViewModel.data.id})).map(x => x.group);
+    await componentInstance.initialize(selectedGroups);
   };
 
 }
