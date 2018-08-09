dda2595be01a3204735be8d4f216e675e22b1bdb
mannyrobin <69haiqian@gmail.com>
2018-08-09 12:37:19 +0300

    SM-177 ~ Environment for school

--- src/environments/environment.school.ts
+++ src/environments/environment.school.ts
@@ -1,5 +1,9 @@
-export const environment = {
+import {IEnvironment} from './ienvironment';
+import {EnvironmentType} from './environment-type';
+
+export const environment: IEnvironment = {
   production: true,
+  type: EnvironmentType.SCHOOL,
   host: 'sch.api.ar.zone',
   restUrl: 'https://sch.api.ar.zone/sp/v2',
   wsUrl: 'https://sch.api.ar.zone/sp/v2/ws',
