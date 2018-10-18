e5b9e6f1815c13a78f30e3c745cac8555bc8b62c
mannyrobin <69haiqian@gmail.com>
2018-10-18 13:23:32 +0300

    SM-230 - EventType

--- src/app/data/local/event-type.ts
+++
@@ -1,6 +0,0 @@
-import {TrainingDiscriminator} from '../remote/model/training/base/training-discriminator';
-
-export class EventType {
-  public name: string;
-  public type: TrainingDiscriminator;
-}
