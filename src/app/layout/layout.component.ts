493ca1d019d011a5cd04360bd8c99922d079ae97
mannyrobin <69haiqian@gmail.com>
2019-01-15 12:58:04 +0300

    SM-267 ~ Visible right sidebar by environment

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,6 +2,9 @@ import {Component, OnDestroy} from '@angular/core';
 import {LayoutService} from '../shared/layout.service';
 import {ISubscription} from 'rxjs-compat/Subscription';
 import {AppHelper} from '../utils/app-helper';
+import {IEnvironment} from '../../environments/ienvironment';
+import {environment} from '../../environments/environment';
+import {EnvironmentType} from '../../environments/environment-type';
 
 @Component({
   selector: 'app-layout',
@@ -10,18 +13,21 @@ import {AppHelper} from '../utils/app-helper';
 })
 export class LayoutComponent implements OnDestroy {
 
+  public readonly environment: IEnvironment;
+  public readonly environmentTypeClass = EnvironmentType;
   public hidden: boolean;
   public dark: boolean;
 
   private readonly _hiddenSubscription: ISubscription;
   private readonly _darkSubscription: ISubscription;
 
-  constructor(private layoutService: LayoutService,
+  constructor(private _layoutService: LayoutService,
               private _appHelper: AppHelper) {
+    this.environment = environment;
     this.hidden = true;
 
-    this._hiddenSubscription = this.layoutService.hidden.subscribe(val => this.hidden = val);
-    this._darkSubscription = this.layoutService.dark.subscribe(value => this.dark = value);
+    this._hiddenSubscription = this._layoutService.hidden.subscribe(val => this.hidden = val);
+    this._darkSubscription = this._layoutService.dark.subscribe(value => this.dark = value);
   }
 
   ngOnDestroy(): void {
