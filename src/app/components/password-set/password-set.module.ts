865085e04b592deef1d80d10f8955c088812158b
mannyrobin <69haiqian@gmail.com>
2019-01-17 16:46:37 +0300

    SM-267 ~ Show notification in reset password

--- src/app/components/password-set/password-set.module.ts
+++ src/app/components/password-set/password-set.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PasswordSetComponent} from './password-set.component';
-import {DxTextBoxModule, DxValidatorModule} from 'devextreme-angular';
 import {TranslateModule} from '@ngx-translate/core';
 import {NgxButtonModule} from '../ngx-button/ngx-button.module';
 import {RouterModule} from '@angular/router';
+import {NgxInputModule} from '../ngx-input/ngx-input.module';
+import {NgxFormModule} from '../ngx-form/ngx-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    DxTextBoxModule,
-    DxValidatorModule,
     TranslateModule.forChild(),
     RouterModule.forChild([]),
-    NgxButtonModule
+    NgxButtonModule,
+    NgxInputModule,
+    NgxFormModule
   ],
   exports: [PasswordSetComponent],
   declarations: [PasswordSetComponent]
