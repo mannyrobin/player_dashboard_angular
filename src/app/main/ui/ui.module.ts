14b9ede20ffb784f48604868c706871a8286eeb1
mannyrobin <69haiqian@gmail.com>
2019-04-23 14:16:31 +0300

    SM-303 - Removed Cards

--- src/app/main/ui/ui.module.ts
+++ src/app/main/ui/ui.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { UICardsModule } from 'app/main/ui/cards/cards.module';
 import { UIFormsModule } from 'app/main/ui/forms/forms.module';
 import { UIIconsModule } from 'app/main/ui/icons/icons.module';
 import { UITypographyModule } from 'app/main/ui/typography/typography.module';
@@ -10,7 +9,6 @@ import { UIColorsModule } from 'app/main/ui/colors/colors.module';
 
 @NgModule({
     imports: [
-        UICardsModule,
         UIFormsModule,
         UIIconsModule,
         UITypographyModule,
