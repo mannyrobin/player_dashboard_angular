7bbc0ba08c3d32938297453a08dbf062f6de9131
mannyrobin <69haiqian@gmail.com>
2019-05-23 16:10:02 +0300

    SM-316 ~ Changed group item style

--- src/app/module/group/group-item/group-item.module.ts
+++ src/app/module/group/group-item/group-item.module.ts
@@ -4,13 +4,17 @@ import {GroupItemComponent} from './group-item/group-item.component';
 import {NgxImageModule} from '../../../components/ngx-image/ngx-image.module';
 import {TranslateModule} from '@ngx-translate/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatCardModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxImageModule,
     TranslateModule.forChild(),
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [GroupItemComponent],
   entryComponents: [GroupItemComponent],
--- src/app/module/person/person-item/person-item.module.ts
+++ src/app/module/person/person-item/person-item.module.ts
@@ -2,17 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PersonItemComponent} from './person-item/person-item.component';
 import {NgxImageModule} from '../../../components/ngx-image/ngx-image.module';
-import {TranslateModule} from '@ngx-translate/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {NgxButtonModule} from '../../../components/ngx-button/ngx-button.module';
 import {MatButtonModule, MatCardModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     NgxImageModule,
-    NgxButtonModule,
-    TranslateModule.forChild(),
     FlexLayoutModule,
     MatCardModule,
     MatButtonModule,
