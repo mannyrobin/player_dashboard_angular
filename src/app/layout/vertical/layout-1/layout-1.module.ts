d7ee8a15138da2e9eece5078a7ed3f27b616542b
mannyrobin <69haiqian@gmail.com>
2019-06-06 12:36:28 +0300

    SM-321 + Added NgxContainer

--- src/app/layout/vertical/layout-1/layout-1.module.ts
+++ src/app/layout/vertical/layout-1/layout-1.module.ts
@@ -1,39 +1,48 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
-import { FuseSidebarModule } from '@fuse/components';
-import { FuseSharedModule } from '@fuse/shared.module';
+import {FuseSidebarModule} from '@fuse/components';
+import {FuseSharedModule} from '@fuse/shared.module';
 
-import { ChatPanelModule } from 'app/layout/components/chat-panel/chat-panel.module';
-import { ContentModule } from 'app/layout/components/content/content.module';
-import { FooterModule } from 'app/layout/components/footer/footer.module';
-import { NavbarModule } from 'app/layout/components/navbar/navbar.module';
-import { QuickPanelModule } from 'app/layout/components/quick-panel/quick-panel.module';
-import { ToolbarModule } from 'app/layout/components/toolbar/toolbar.module';
+import {ChatPanelModule} from 'app/layout/components/chat-panel/chat-panel.module';
+import {ContentModule} from 'app/layout/components/content/content.module';
+import {FooterModule} from 'app/layout/components/footer/footer.module';
+import {NavbarModule} from 'app/layout/components/navbar/navbar.module';
+import {QuickPanelModule} from 'app/layout/components/quick-panel/quick-panel.module';
+import {ToolbarModule} from 'app/layout/components/toolbar/toolbar.module';
 
-import { VerticalLayout1Component } from 'app/layout/vertical/layout-1/layout-1.component';
+import {VerticalLayout1Component} from 'app/layout/vertical/layout-1/layout-1.component';
+import {NgxContainerModule} from '../../../module/ngx/ngx-container/ngx-container.module';
+import {ConversationsModule} from '../../../module/conversation/conversations/conversations.module';
+import {ConversationViewModule} from '../../../module/conversation/conversation-view/conversation-view.module';
+import {MatIconModule} from '@angular/material';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-    declarations: [
-        VerticalLayout1Component
-    ],
-    imports     : [
-        RouterModule,
+  declarations: [
+    VerticalLayout1Component
+  ],
+  imports: [
+    RouterModule,
 
-        FuseSharedModule,
-        FuseSidebarModule,
+    FuseSharedModule,
+    FuseSidebarModule,
 
-        ChatPanelModule,
-        ContentModule,
-        FooterModule,
-        NavbarModule,
-        QuickPanelModule,
-        ToolbarModule
-    ],
-    exports     : [
-        VerticalLayout1Component
-    ]
+    ChatPanelModule,
+    ContentModule,
+    FooterModule,
+    NavbarModule,
+    QuickPanelModule,
+    ToolbarModule,
+    NgxContainerModule,
+    ConversationsModule,
+    ConversationViewModule,
+    MatIconModule,
+    TranslateModule.forChild()
+  ],
+  exports: [
+    VerticalLayout1Component
+  ]
 })
-export class VerticalLayout1Module
-{
+export class VerticalLayout1Module {
 }
---
+++ src/app/module/ngx/ngx-container/ngx-container.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NgxContainerComponent} from './ngx-container/ngx-container.component';
+import {MatIconModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [NgxContainerComponent],
+  exports: [NgxContainerComponent],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    FlexLayoutModule
+  ]
+})
+export class NgxContainerModule {
+}
