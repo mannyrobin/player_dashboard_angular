f764762158c5b5096fddc7eb2b28c78684185af9
mannyrobin <69haiqian@gmail.com>
2019-06-11 18:40:10 +0300

    SM-322 + Added units list

---
+++ src/app/pages/dictionary/units/units/units.component.ts
@@ -0,0 +1,10 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-units',
+  templateUrl: './units.component.html',
+  styleUrls: ['./units.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UnitsComponent {
+}
