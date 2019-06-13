e7cc2fdd52f56ca646b3cb42ab0ca0bcd051487c
mannyrobin <69haiqian@gmail.com>
2019-06-14 00:10:29 +0300

    SM-322 + Added header for parameters and units

--- src/app/pages/dictionary/parameters/parameters.module.ts
+++ src/app/pages/dictionary/parameters/parameters.module.ts
@@ -3,13 +3,15 @@ import {CommonModule} from '@angular/common';
 import {ParametersComponent} from './parameters/parameters.component';
 import {ParameterListModule} from '../../../module/parameter/parameter-list/parameter-list.module';
 import {ParametersRoutingModule} from './parameters-routing.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ParametersComponent],
   imports: [
     CommonModule,
     ParametersRoutingModule,
-    ParameterListModule
+    ParameterListModule,
+    TranslateModule.forChild()
   ]
 })
 export class ParametersModule {
--- src/app/pages/dictionary/units/units.module.ts
+++ src/app/pages/dictionary/units/units.module.ts
@@ -3,13 +3,15 @@ import {CommonModule} from '@angular/common';
 import {UnitsRoutingModule} from './units-routing.module';
 import {UnitsComponent} from './units/units.component';
 import {UnitListModule} from '../../../module/unit/unit-list/unit-list.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [UnitsComponent],
   imports: [
     CommonModule,
     UnitsRoutingModule,
-    UnitListModule
+    UnitListModule,
+    TranslateModule.forChild()
   ]
 })
 export class UnitsModule {
