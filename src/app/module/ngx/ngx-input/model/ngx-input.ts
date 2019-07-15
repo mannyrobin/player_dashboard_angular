c1d6c830f3059c87bc8898f7365b729c60e13c80
mannyrobin <69haiqian@gmail.com>
2019-07-15 17:26:14 +0300

    SM-333 ~ Updated NgxInput

--- src/app/module/ngx/ngx-input/model/ngx-input.ts
+++ src/app/module/ngx/ngx-input/model/ngx-input.ts
@@ -2,14 +2,17 @@ import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {MatFormFieldAppearance} from '@angular/material/typings/form-field';
 import {NgxInputType} from './ngx-input-type';
+import {FloatLabelType} from '@angular/material';
 
 export class NgxInput {
   public appearance: MatFormFieldAppearance = 'legacy';
+  public floatLabel: FloatLabelType = 'always';
   public type = NgxInputType.TEXT;
   public control = new FormControl();
   public label?: string;
   public labelTranslation?: string;
   public placeholder?: string;
+  public placeholderTranslation?: string;
   public showClearButton?: boolean;
   public rows?: number;
   // TODO: Get dynamically from FormControl
