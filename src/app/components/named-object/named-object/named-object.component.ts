b87bd8d81b518fbcfeef2cb3f2ebbd2ed7a8c5e0
mannyrobin <69haiqian@gmail.com>
2019-07-18 16:17:55 +0300

    SM-333 ~ Updated NamedObject

--- src/app/components/named-object/named-object/named-object.component.ts
+++ src/app/components/named-object/named-object/named-object.component.ts
@@ -1,13 +1,17 @@
-import {Component, Input, ViewChild} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NamedObject} from '../../../data/remote/base/named-object';
 import {DxTextBoxComponent} from 'devextreme-angular';
+import {NgxInput} from '../../../module/ngx/ngx-input/model/ngx-input';
+import {FormGroup, Validators} from '@angular/forms';
+import {takeWhile} from 'rxjs/operators';
+import {NgxInputType} from '../../../module/ngx/ngx-input/model/ngx-input-type';
 
 @Component({
   selector: 'app-named-object',
   templateUrl: './named-object.component.html',
   styleUrls: ['./named-object.component.scss']
 })
-export class NamedObjectComponent<T extends NamedObject> {
+export class NamedObjectComponent<T extends NamedObject> implements OnInit, OnDestroy {
 
   @ViewChild('name')
   public name: DxTextBoxComponent;
@@ -15,12 +19,42 @@ export class NamedObjectComponent<T extends NamedObject> {
   @Input()
   public data: T;
 
+  public readonly nameNgxInput = new NgxInput();
+  public readonly descriptionNgxInput = new NgxInput();
+  public readonly formGroup = new FormGroup({});
+  private _notDestroyed = true;
+
   constructor() {
     this.data = <T>{};
   }
 
+  public ngOnInit(): void {
+    this.nameNgxInput.labelTranslation = 'name';
+    this.nameNgxInput.required = true;
+    this.nameNgxInput.control.setValidators(Validators.required);
+    this.nameNgxInput.control.setValue(this.data.name);
+
+    this.descriptionNgxInput.labelTranslation = 'description';
+    this.descriptionNgxInput.type = NgxInputType.TEXTAREA;
+    this.descriptionNgxInput.control.setValue(this.data.description);
+
+    this.formGroup.setControl('name', this.nameNgxInput.control);
+    this.formGroup.setControl('description', this.descriptionNgxInput.control);
+
+    this.formGroup.valueChanges
+      .pipe(takeWhile(() => this._notDestroyed))
+      .subscribe((value) => {
+        this.data.name = value.name;
+        this.data.description = value.description;
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this._notDestroyed = false;
+  }
+
   public valid(): boolean {
-    return this.name.validator.instance.validate().isValid;
+    return this.formGroup.valid;
   }
 
 }
--- src/app/components/named-object/preview-named-object/preview-named-object.component.ts
+++ src/app/components/named-object/preview-named-object/preview-named-object.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {BaseComponent} from '../../../data/local/component/base/base-component';
 import {NamedObject} from '../../../data/remote/base/named-object';
 
 @Component({
   selector: 'app-preview-named-object',
   templateUrl: './preview-named-object.component.html',
-  styleUrls: ['./preview-named-object.component.scss']
+  styleUrls: ['./preview-named-object.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PreviewNamedObjectComponent<T extends NamedObject | any> extends BaseComponent<T> {
 
