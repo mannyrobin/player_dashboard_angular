8170010af2236570e1c62cd9053c195b61397994
mannyrobin <69haiqian@gmail.com>
2019-07-23 12:41:19 +0300

    SM-340 ! Fixed group person item style

--- src/app/module/group/group-person-item/group-person-item.module.ts
+++ src/app/module/group/group-person-item/group-person-item.module.ts
@@ -3,10 +3,15 @@ import {CommonModule} from '@angular/common';
 import {GroupPersonItemComponent} from './group-person-item/group-person-item.component';
 import {PersonItemModule} from '../../person/person-item/person-item.module';
 import {TranslateModule} from '@ngx-translate/core';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
     TranslateModule.forChild(),
     PersonItemModule
   ],
