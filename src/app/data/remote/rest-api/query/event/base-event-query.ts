9cd4e1db91e367dc4eb5f980f6e9f570ec021635
mannyrobin <69haiqian@gmail.com>
2019-07-30 18:53:39 +0300

    SM-336 ! Fixed calendar filter

--- src/app/data/remote/rest-api/query/event/base-event-query.ts
+++ src/app/data/remote/rest-api/query/event/base-event-query.ts
@@ -3,6 +3,7 @@ import {PageQuery} from '../../page-query';
 export class BaseEventQuery extends PageQuery {
   public startDate?: Date;
   public finishDate?: Date;
+  public groupIds?: string;
 
   // For EventGroup
   public unassigned?: boolean;
