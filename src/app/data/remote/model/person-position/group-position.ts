f6c252df7d4f37db693909ca60d877626334abec
mannyrobin <69haiqian@gmail.com>
2019-08-28 12:44:34 +0300

    SM-397 ~ Minor changes

--- src/app/data/remote/model/person-position/group-position.ts
+++ src/app/data/remote/model/person-position/group-position.ts
@@ -1,13 +1,9 @@
 import {BasePosition} from './base-position';
 import {PositionType} from './position-type';
-import {Group} from '../group/base/group';
 import {Type} from 'class-transformer';
 import {Position} from './position';
 
 export class GroupPosition extends BasePosition {
-
-  @Type(() => Group)
-  public group: Group;
 
   @Type(() => Position)
   public relevantPosition: Position;
