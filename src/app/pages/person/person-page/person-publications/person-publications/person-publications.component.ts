a6cf354a5a65aeb176af6cdd267505f646135060
mannyrobin <69haiqian@gmail.com>
2019-09-11 15:39:02 +0300

    SM-424 ! Fixed showing news in person profile

--- src/app/pages/person/person-page/person-publications/person-publications/person-publications.component.ts
+++ src/app/pages/person/person-page/person-publications/person-publications/person-publications.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {BasePersonComponent} from '../../../model/base-person-component';
 import {PersonService} from '../../service/person.service';
+import {Person} from '../../../../../data/remote/model/person';
+import {AuthorizationService} from '../../../../../shared/authorization.service';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-person-publications',
@@ -8,7 +11,26 @@ import {PersonService} from '../../service/person.service';
   styleUrls: ['./person-publications.component.scss']
 })
 export class PersonPublicationsComponent extends BasePersonComponent {
-  constructor(personService: PersonService) {
+
+  public personNews: Person;
+
+  constructor(private _authorizationService: AuthorizationService,
+              personService: PersonService) {
     super(personService);
   }
+
+  public updatePerson(person: Person): void {
+    super.updatePerson(person);
+
+    this._authorizationService.person$
+      .pipe(take(1))
+      .subscribe(value => {
+        if (value && person && value.id != person.id) {
+          this.personNews = person;
+        } else {
+          delete this.personNews;
+        }
+      });
+  }
+
 }
