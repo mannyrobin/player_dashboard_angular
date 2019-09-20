d5629556da42128ea4cf8e0e3850fb4d56c48cdb
mannyrobin <69haiqian@gmail.com>
2019-09-20 16:29:46 +0300

    SM-344 + Added adding a group in contact and search pages

--- src/app/pages/search/groups/groups/groups.component.ts
+++ src/app/pages/search/groups/groups/groups.component.ts
@@ -1,25 +1,40 @@
-import {Component, ViewChild} from '@angular/core';
-import {ItemDisplay} from '../../../../module/common/item-list/model/item-display';
-import {GroupsListComponent} from '../../../../module/group/groups-list/groups-list/groups-list.component';
-import {GroupQuery} from '../../../../data/remote/rest-api/query/group-query';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Group } from '../../../../data/remote/model/group/base';
+import { GroupQuery } from '../../../../data/remote/rest-api/query/group-query';
+import { MenuItem } from '../../../../module/common/item-line/model/menu-item';
+import { ItemDisplay } from '../../../../module/common/item-list/model/item-display';
+import { GroupsListComponent } from '../../../../module/group/groups-list/groups-list/groups-list.component';
+import { TemplateModalService } from '../../../../service/template-modal.service';
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
   styleUrls: ['./groups.component.scss']
 })
-export class GroupsComponent {
+export class GroupsComponent implements OnInit {
 
   @ViewChild(GroupsListComponent, { static: false })
   public _groupsListComponent: GroupsListComponent;
 
   public readonly groupQuery: GroupQuery;
   public itemDisplay: ItemDisplay;
+  public actions: MenuItem[];
 
-  constructor() {
+  constructor(private _templateModalService: TemplateModalService) {
     this.groupQuery = {
       name: ''
     };
+  }
+
+  public ngOnInit(): void {
+    this.actions = [
+      {
+        iconName: 'add',
+        action: async () => {
+          await this._templateModalService.showEditGroupModal(new Group());
+        }
+      }
+    ];
   }
 
   public async onSearchTextChange(value: string): Promise<void> {
