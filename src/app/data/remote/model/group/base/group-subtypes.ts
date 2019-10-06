f790d5745a3758df8901a53c9456e3118ea0ca91
mannyrobin <69haiqian@gmail.com>
2019-10-06 19:15:55 +0300

    SM-435 ! Fixed production build

--- src/app/data/remote/model/group/base/group-subtypes.ts
+++ src/app/data/remote/model/group/base/group-subtypes.ts
@@ -1,16 +1,9 @@
-import { Agency } from '../agency';
-import { Community } from '../community';
-import { IntervalGroup } from '../interval';
-import { Location } from '../location';
-import { Organization } from '../organization';
-import { Team } from '../team';
-import { GroupTypeEnum } from './group-type-enum';
-
 export const GROUP_SUBTYPES = [
-  {value: Team, name: GroupTypeEnum.TEAM},
-  {value: Community, name: GroupTypeEnum.COMMUNITY},
-  {value: Organization, name: GroupTypeEnum.ORGANIZATION},
-  {value: Agency, name: GroupTypeEnum.AGENCY},
-  {value: Location, name: GroupTypeEnum.LOCATION},
-  {value: IntervalGroup, name: GroupTypeEnum.INTERVAL_GROUP}
+  // TODO: Fix typization
+  // {value: Team, name: GroupTypeEnum.TEAM},
+  // {value: Community, name: GroupTypeEnum.COMMUNITY},
+  // {value: Organization, name: GroupTypeEnum.ORGANIZATION},
+  // {value: Agency, name: GroupTypeEnum.AGENCY},
+  // {value: Location, name: GroupTypeEnum.LOCATION},
+  // {value: IntervalGroup, name: GroupTypeEnum.INTERVAL_GROUP}
 ];
