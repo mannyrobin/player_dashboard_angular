bbdb26a94bca19bd8322cd094dd20a5c5b3ff376
mannyrobin <69haiqian@gmail.com>
2019-10-17 17:39:24 +0300

    SM-442 ~ Updated search page

--- src/app/pages/search/search-routing.module.ts
+++ src/app/pages/search/search-routing.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {SearchComponent} from './search/search.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {
