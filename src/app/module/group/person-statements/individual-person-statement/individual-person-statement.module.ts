74c64c0242c8a42636548f3560b3389fa8040c97
mannyrobin <69haiqian@gmail.com>
2019-11-26 21:54:41 +0300

    SM-476 ~ Updated claim request and person profile

--- src/app/module/group/person-statements/individual-person-statement/individual-person-statement.module.ts
+++ src/app/module/group/person-statements/individual-person-statement/individual-person-statement.module.ts
@@ -6,7 +6,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NgxGridModule } from 'app/components/ngx-grid/ngx-grid.module';
 import { NgxImageModule } from 'app/components/ngx-image';
 import { EditGroupPersonClaimStateModule } from 'app/module/group/edit-group-person-claim-state/edit-group-person-claim-state.module';
-import { GroupPersonRequestModule } from 'app/module/group/group-person-request/group-person-request.module';
 import { NgxDateModule } from 'app/module/ngx/ngx-date/ngx-date.module';
 import { NgxInputModule } from 'app/module/ngx/ngx-input';
 import { NgxSelectModule } from 'app/module/ngx/ngx-select/ngx-select.module';
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './ienvironment';
 
 export const environment: IEnvironment = {
   production: true,
-  version: '1.1.1-52',
+  version: '1.1.1-55',
   type: EnvironmentType.TEST,
   host: 'test.api.ar.zone',
   restUrl: 'https://test.api.ar.zone/sp/v2',
